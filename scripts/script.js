cd8dce75bcee9c6817be43428c3b222aa44b9d6f
MisterBrash <matthew.brash@ocsb.ca>
2024-10-04 01:54:22 +0000

    First Draft

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/**
+ * 2.1 - Console vs. Document
+ * ICS3 - Mr. Brash 🐿️
+ * 
+ * What you see here is a block comment. More on that later. READ THE INSTRUCTIONS
+ **/ 
+
+console.log("Hello world!")
